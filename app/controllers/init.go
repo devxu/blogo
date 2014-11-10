e284ebf85848dcee4bbcc0702012eb74cc0c9fb8
devxu <admin@devxu.com>
2014-11-10 15:48:07 +0800

    修改自定义模板初始化位置

--- app/controllers/init.go
+++ app/controllers/init.go
@@ -1,10 +1,39 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/revel/revel"
+	"regexp"
+	"time"
+	"unicode/utf8"
 )
 
 func init() {
 
 	revel.InterceptMethod((*AppController).checkAuth, revel.BEFORE)
+
+	addTemplateFun()
+}
+
+//添加模板函数
+func addTemplateFun() {
+
+	revel.TemplateFuncs["delHtml"] = func(text string) string {
+		if text != "" {
+			reg, _ := regexp.Compile("<(.[^>]*)>")
+			text = reg.ReplaceAllString(text, "")
+			if utf8.RuneCountInString(text) > 200 {
+				text = string([]rune(text)[0:200]) + "..."
+			}
+		}
+		return text
+	}
+
+	revel.TemplateFuncs["randAvatar"] = func(n int64) string {
+		return fmt.Sprintf("%d.jpg", n%3+1)
+	}
+
+	revel.TemplateFuncs["timeFormat"] = func(t time.Time, layout string) string {
+		return t.Format(layout)
+	}
 }
--- app/routes/routes.go
+++ app/routes/routes.go
@@ -38,36 +38,6 @@ func (_ tStatic) ServeModule(
 }
 
 
-type tTestRunner struct {}
-var TestRunner tTestRunner
-
-
-func (_ tTestRunner) Index(
-		) string {
-	args := make(map[string]string)
-	
-	return revel.MainRouter.Reverse("TestRunner.Index", args).Url
-}
-
-func (_ tTestRunner) Run(
-		suite string,
-		test string,
-		) string {
-	args := make(map[string]string)
-	
-	revel.Unbind(args, "suite", suite)
-	revel.Unbind(args, "test", test)
-	return revel.MainRouter.Reverse("TestRunner.Run", args).Url
-}
-
-func (_ tTestRunner) List(
-		) string {
-	args := make(map[string]string)
-	
-	return revel.MainRouter.Reverse("TestRunner.List", args).Url
-}
-
-
 type tAdmin struct {}
 var Admin tAdmin
 
